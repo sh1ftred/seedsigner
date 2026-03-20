460574e5567b532f51db8c2494075b0692d353e1
redshift <213178690+1ftredsh@users.noreply.github.com>
2026-03-20 10:26:15 +0000

    Move Nostr menu into Tools

--- src/seedsigner/views/tools_views.py
+++ src/seedsigner/views/tools_views.py
@@ -25,9 +25,10 @@ class ToolsMenuView(View):
     KEYBOARD = ButtonOption("Calc 12th/24th word", FontAwesomeIconConstants.KEYBOARD)
     ADDRESS_EXPLORER = ButtonOption("Address explorer")
     VERIFY_ADDRESS = ButtonOption("Verify address")
+    NOSTR = ButtonOption("Nostr")
 
     def run(self):
-        button_data = [self.IMAGE, self.DICE, self.KEYBOARD, self.ADDRESS_EXPLORER, self.VERIFY_ADDRESS]
+        button_data = [self.IMAGE, self.DICE, self.KEYBOARD, self.ADDRESS_EXPLORER, self.VERIFY_ADDRESS, self.NOSTR]
 
         selected_menu_num = self.run_screen(
             ButtonListScreen,
@@ -54,6 +55,10 @@ class ToolsMenuView(View):
         elif button_data[selected_menu_num] == self.VERIFY_ADDRESS:
             from seedsigner.views.scan_views import ScanAddressView
             return Destination(ScanAddressView)
+
+        elif button_data[selected_menu_num] == self.NOSTR:
+            from seedsigner.views.nostr_views import NostrMenuView
+            return Destination(NostrMenuView)
 
 
 
